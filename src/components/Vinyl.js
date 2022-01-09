608894ff49df4680a0a4f6fc653e20956af6cfa0
ekaterinaMelnik <melnykkatia@gmail.com>
2022-01-09 15:10:52 +0200

    feat: select label depending on loadingState

--- src/components/Vinyl.js
+++ src/components/Vinyl.js
@@ -2,29 +2,42 @@ import { useEffect, useRef } from "react";
 import { Animate } from '../utils/Animate';
 import LOADING_STATE from '../constants/loadingState';
 
+const getLabel = (loading) => {
+  switch (loading) {
+    case LOADING_STATE.LOADING:
+      return 'Loading';
+    case LOADING_STATE.LOADED:
+      return 'Copied';
+    default:
+      return 'Copy';
+  }
+}
+
 export function Vinyl({ loading, onClick }) {
-    const animation = useRef();
+  const animation = useRef();
 
-    useEffect(() => {
-        animation.current = new Animate('#btn');
-    }, []);
+  const label = getLabel(loading);
 
-    useEffect(() => {
-        switch (loading) {
-            case LOADING_STATE.LOADING: 
-                animation.current.start();
-                break;
-            case LOADING_STATE.LOADED:
-                animation.current.next();
-                break;
-            default:
-                break;
-        }
-    }, [loading]);
+  useEffect(() => {
+    animation.current = new Animate('#btn');
+  }, []);
 
-    return (
-        <button id="btn" class="vinyl" onClick={onClick}>
-            <div class="vinyl-text">Copy</div>
-        </button>
-    )
+  useEffect(() => {
+    switch (loading) {
+      case LOADING_STATE.LOADING:
+        animation.current.start();
+        break;
+      case LOADING_STATE.LOADED:
+        animation.current.next();
+        break;
+      default:
+        break;
+    }
+  }, [loading]);
+
+  return (
+    <button id="btn" className="vinyl" onClick={onClick}>
+      <div className="vinyl-text">{label}</div>
+    </button>
+  )
 }
